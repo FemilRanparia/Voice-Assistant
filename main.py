67b8f8fae42c6d23050643269e917d39c8f78fa4
Femil Ranparia <femilranparia@gmail.com>
2025-06-12 19:13:46 +0530

    Greet based on time

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ import musicLibrary
 
 def voice_assistant():
     # recognizer = sr.Recognizer()
-    engine = pyttsx3.init()
 
     def speak(text):
+        engine = pyttsx3.init()
+        engine.setProperty('rate', 170)
         engine.say(text)
         engine.runAndWait()
     # This function is use to convert text to speech
@@ -41,33 +42,35 @@ def voice_assistant():
         else:
             speak("Please Speak Again")
 
-    speak("Initializing Jarvis...")
+    # speak("Initializing Jarvis...")
     r = sr.Recognizer()
+    # speak("Jarvis Activated")
+    # speak("Jarvis Initialized")
     # while True:
     # Listen for the wake word jarvis
     # obtain audio from microphone
     print("recognizing...")
     try:
-        with sr.Microphone() as source:
-            r.adjust_for_ambient_noise(source, duration=1)
-            print("Listening...")
-            audio = r.listen(source, timeout=5, phrase_time_limit=3)
+        # with sr.Microphone() as source:
+            # r.adjust_for_ambient_noise(source, duration=1)
+            # print("Listening...")
+            # audio = r.listen(source, timeout=5, phrase_time_limit=4)
             # above code is for listining from microphone
             # timeout is time it will listen. after 2 secs it will do timeout
             # phasetimeout is time after how much sec u take to continue
-        word = r.recognize_google(audio)
+        # word = r.recognize_google(audio)
         # print(word)
-        if "jarvis" in word.lower():
-            speak("I am listening")
+        # if "jarvis" in word.lower():
+            # speak("I am listening")
             # Listen for the further command
-            with sr.Microphone() as source:
-                print("Jarvis Active")
-                audio = r.listen(source)
-                command = r.recognize_google(audio)
-                processCommand(command)
+        with sr.Microphone() as source:
+            speak("Jarvis Activated")
+            audio = r.listen(source, timeout=5, phrase_time_limit=5)
+            command = r.recognize_google(audio)
+            processCommand(command)
 
     except Exception as e:
         print("Error ; {0}".format(e))
 
 if __name__ == "__main__":
-    voice_assistant()
+    voice_assistant()   
--- startup.py
+++ startup.py
@@ -1,6 +1,35 @@
 from main import voice_assistant
-
 import keyboard
+import pyttsx3
+from datetime import datetime
+# import threading
+
+
+def speak(text):    
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 170)
+    engine.say(text)
+    engine.runAndWait()
+
+def greet(hour):
+    if hour>12 and hour<16:
+        speak("Good Afternoon Femil Please Press 's' to Activate Jarvis")
+    elif hour>16 and hour<24:
+        speak("Good Evening Femil Please Press 's' to Activate Jarvis")
+    else:
+        speak("Good Morning Femil Please Press 's' to Activate Jarvis")
+
+now = datetime.now()
+
+curr_hour = int(now.strftime("%H"))
+
+greet(curr_hour)
+
+
+# def trigger_jarvis():
+#     t = threading.Thread(target=voice_assistant)
+#     t.daemon = False
+#     t.start()
 
 keyboard.add_hotkey('s', voice_assistant)
 
