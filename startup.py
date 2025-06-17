835226c83438b958e31501b07986eadd5cad1ff4
FemilRanparia <femilranparia@gmail.com>
2025-06-17 11:42:15 +0530

    gpt model included

--- startup.py
+++ startup.py
@@ -2,8 +2,7 @@ from main import voice_assistant
 import keyboard
 import pyttsx3
 from datetime import datetime
-# import threading
-
+import random
 
 def speak(text):    
     engine = pyttsx3.init()
@@ -12,12 +11,16 @@ def speak(text):
     engine.runAndWait()
 
 def greet(hour):
-    if hour>12 and hour<16:
-        speak("Good Afternoon Femil Please Press 's' to Activate Jarvis")
+
+    names = ['Femil', 'Operator', 'Chief', 'Captian']
+    name = random.choice(names)
+
+    if hour>=12 and hour<=16:
+        speak(f"Good Afternoon {name} Please Press 's' to Activate Jarvis")
     elif hour>16 and hour<24:
-        speak("Good Evening Femil Please Press 's' to Activate Jarvis")
+        speak(f"Good Evening {name} Please Press 's' to Activate Jarvis")
     else:
-        speak("Good Morning Femil Please Press 's' to Activate Jarvis")
+        speak(f"Good Morning {name} Please Press 's' to Activate Jarvis")
 
 now = datetime.now()
 
@@ -25,12 +28,11 @@ curr_hour = int(now.strftime("%H"))
 
 greet(curr_hour)
 
-
-# def trigger_jarvis():
-#     t = threading.Thread(target=voice_assistant)
-#     t.daemon = False
-#     t.start()
-
 keyboard.add_hotkey('s', voice_assistant)
+
+key = keyboard.get_hotkey_name()
+
+if key=='esc':
+    speak("GoodBye")
 
 keyboard.wait('esc')
